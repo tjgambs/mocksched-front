e1c950e0c72b8507fa7273f2231809487cfc2f5d
Tim <tjgambs@gmail.com>
2017-06-03 21:58:08 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <other></other>
+</template>
+
+<script>
+
+  import OtherComponent from './otherComponent.vue'
+
+  export default {    
+    components: {
+      'other': OtherComponent
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
